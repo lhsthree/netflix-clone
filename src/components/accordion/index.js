d952340aafc41eec58da9a51166fd416440d5461
lhsthree <lhsthree@gmail.com>
2020-10-28 19:21:50 -0500

    feat: styling of accordion

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Item, Container, Inner, Title, Header, Body } from './styles/accordion'
+import { Frame, Item, Container, Inner, Title, Header, Body } from './styles/accordion'
 
 export default function Accordion({ children, ...restProps }) {
     return (
@@ -7,6 +7,10 @@ export default function Accordion({ children, ...restProps }) {
             <Inner>{children}</Inner>
         </Container>
     )
+}
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+    return <Frame {...restProps}>{children}</Frame>;
 }
 
 Accordion.Item  = function AccordionItem({ children, ...restProps}) {
